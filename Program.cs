7bec856c773aae2aa5d13a06c192e7061ea536bf
porushmarwaha <porushmarwaha@gmail.com>
2020-07-24 23:33:45 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Dotnet {
+	class Program {
+		static void Main (string[] args) {
+
+			Console.Write ("Please type number: ");
+			Console.WriteLine ("The Number is - {0}", EvenOddFunction (int.Parse (Console.ReadLine ())));
+
+			Console.Write("Plese Type String: ");
+			Console.WriteLine ("The Sum is - {0}", CalculateCommaSeperatedValues (Console.ReadLine()));
+
+		}
+		private static int CalculateCommaSeperatedValues (string commaSeperatedString) {
+			int sum = 0;
+			foreach (string item in commaSeperatedString.Split (',')) {
+				if (int.Parse (item) < 0) continue;
+				sum += int.Parse (item);
+			}
+			return sum;
+		}
+		private static string EvenOddFunction (int number) { return (number % 2 == 0) ? "Even" : "Odd"; }
+
+	}
+}
