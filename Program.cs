3f80fee6c473076642d78c57ee67bc1682654e13
porushmarwaha <porushmarwaha@gmail.com>
2020-07-27 13:09:35 +0530

    new Problem 4 is added in existing code

--- Program.cs
+++ Program.cs
@@ -13,6 +13,16 @@ namespace Dotnet {
 				if (element.Contains (rememberSpell)) returnedSpells.Add (element);
 			});
 			returnedSpells.ForEach(element => Console.WriteLine(element));
+
+
+			string message = "Have you finished your warewolves essay? ";
+			string secretMessage = message
+			.Replace('a','@')
+			.Replace('e','#')
+			.Replace('i','*')
+			.Replace('o','?')
+			.Replace('u','&');
+			Console.WriteLine(secretMessage); 
 		}
 
 
