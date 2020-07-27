9558621dd96f33f5a3306f89ba7b38351dc3ff5e
porushmarwaha <porushmarwaha@gmail.com>
2020-07-27 17:29:49 +0530

    Problem5 Refactor Code

--- Program.cs
+++ Program.cs
@@ -7,45 +7,41 @@ namespace Dotnet {
 
 			char[] balanced = { '{', '[', ']', '{', '}', '(', '{', '}', ')', '}' };
 			char[] unbalanced = { '{', '}', '(', '{', '(', '}', ')', ')' };
-			
-			Console.WriteLine ("String - {0}" ,CheckBalanceOrUnbalanceString (balanced));
-			Console.WriteLine ("String - {0}" ,CheckBalanceOrUnbalanceString (unbalanced));
 
+			Console.WriteLine ("String - {0}", CheckBalanceOrUnbalanceString (balanced));
+			Console.WriteLine ("String - {0}", CheckBalanceOrUnbalanceString (unbalanced));
 		}
 		private static string CheckBalanceOrUnbalanceString (char[] brackets) {
-			if(brackets.Length ==0) return "No Elements in the array";
-			char[] bracketIndex = { '[', ']', '{', '}', '(', ')' }; 
+			if (brackets.Length == 0) return "No Elements in the array";
+			char[] bracketIndex = { '[', ']', '{', '}', '(', ')' };
 			Stack<char> bracketStack = new Stack<char> ();
 			foreach (char bracket in brackets) {
-				
 				if ((bracketStack.Count == 0) && (
-					bracket == bracketIndex[1] ||
-					bracket == bracketIndex[3] ||
-					bracket == bracketIndex[5]))
+						bracket == bracketIndex[1] ||
+						bracket == bracketIndex[3] ||
+						bracket == bracketIndex[5]))
 					return "UnBalanced";
-			
+
 				// Empty List and First Character
-				if(bracketStack.Count == 0){ bracketStack.Push(bracket); continue;};
-				
+				if (bracketStack.Count == 0) { bracketStack.Push (bracket); continue; };
+
 				// Empty List and First Character
 				if (bracket == bracketIndex[0] ||
 					bracket == bracketIndex[2] ||
-					bracket == bracketIndex[4]){
-					bracketStack.Push(bracket);
+					bracket == bracketIndex[4]) {
+					bracketStack.Push (bracket);
 					continue;
-					}
+				}
 
 				// Check this condition last open bracket '[' and next is closed ']'
 				if (bracketStack.Peek () == bracketIndex[0] && bracket == bracketIndex[1] ||
 					bracketStack.Peek () == bracketIndex[2] && bracket == bracketIndex[3] ||
-					bracketStack.Peek () == bracketIndex[4] && bracket == bracketIndex[5] )
-					bracketStack.Pop(); 
+					bracketStack.Peek () == bracketIndex[4] && bracket == bracketIndex[5])
+					bracketStack.Pop ();
 				else return "UnBalanced";
-
 			}
-			if(bracketStack.Count != 0) return "Unbalanced";
-		return "Balanced";
+			if (bracketStack.Count != 0) return "Unbalanced";
+			return "Balanced";
 		}
-
 	}
 }
