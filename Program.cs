08a74a89eb8bf3f32ed15769f78c831f46f2476e
porushmarwaha <porushmarwaha@gmail.com>
2020-07-27 17:13:14 +0530

    Bracket Balancer Check Program implemented

--- Program.cs
+++ Program.cs
@@ -5,27 +5,46 @@ namespace Dotnet {
 	class Program {
 		static void Main (string[] args) {
 
-			//Problem 3
+			char[] balanced = { '{', '[', ']', '{', '}', '(', '{', '}', ')', '}' };
+			char[] unbalanced = { '{', '}', '(', '{', '(', '}', ')', ')' };
+			char[] test = {};
+			// Console.WriteLine ("String - {0}" ,CheckBalanceOrUnbalanceString (balanced));
+			// Console.WriteLine ("String - {0}" ,CheckBalanceOrUnbalanceString (unbalanced));
+			Console.WriteLine(CheckBalanceOrUnbalanceString(test));
 
-			List<string> listOfSpells = new List<string> () { "Accio", "Expecto Patronum", "Petrificus Totalus", "Muffliato" };
-			string rememberSpell = "tr";
-			List<string> returnedSpells = new List<string> ();
-			if (rememberSpell != "") {
-				listOfSpells.ForEach (element => {
-					if (element.Contains (rememberSpell) == true) returnedSpells.Add (element);
-				});
-				returnedSpells.ForEach (element => Console.WriteLine (element));
-			} else { Console.WriteLine ("Spell Does not Exist"); }
+		}
+		private static string CheckBalanceOrUnbalanceString (char[] brackets) {
+			char[] bracketIndex = { '[', ']', '{', '}', '(', ')' };
+			Stack<char> bracketStack = new Stack<char> ();
+			foreach (char bracket in brackets) {
+				
+				if ((bracketStack.Count == 0) && (
+					bracket == bracketIndex[1] ||
+					bracket == bracketIndex[3] ||
+					bracket == bracketIndex[5]))
+					return "UnBalanced";
+			
+				// Empty List and First Character
+				if(bracketStack.Count == 0){ bracketStack.Push(bracket); continue;};
+				
+				// Empty List and First Character
+				if (bracket == bracketIndex[0] ||
+					bracket == bracketIndex[2] ||
+					bracket == bracketIndex[4]){
+					bracketStack.Push(bracket);
+					continue;
+					}
 
-			//Problrem 4
-			string message = "Have you finished your warewolves essay? ";
-			string secretMessage = message
-				.Replace ('a', '@')
-				.Replace ('e', '#')
-				.Replace ('i', '*')
-				.Replace ('o', '?')
-				.Replace ('u', '&');
-			Console.WriteLine (secretMessage);
+				// Check this condition last open bracket '[' and next is closed ']'
+				if (bracketStack.Peek () == bracketIndex[0] && bracket == bracketIndex[1] ||
+					bracketStack.Peek () == bracketIndex[2] && bracket == bracketIndex[3] ||
+					bracketStack.Peek () == bracketIndex[4] && bracket == bracketIndex[5] )
+					bracketStack.Pop(); 
+				else return "UnBalanced";
+
+			}
+			if(bracketStack.Count != 0) return "Unbalanced";
+		return "Balanced";
 		}
 
 	}
