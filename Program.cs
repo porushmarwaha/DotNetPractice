fdee2d48d12392d01fad16b9e5bb9957f3626613
porushmarwaha <porushmarwaha@gmail.com>
2020-07-27 13:41:36 +0530

    new Problem 4 is added in existing code & Problem3 is refactor

--- Program.cs
+++ Program.cs
@@ -5,26 +5,26 @@ namespace Dotnet {
 	class Program {
 		static void Main (string[] args) {
 
-			List<string> listOfSpells = new List<string> () 
-				{ "Accio", "Expecto Patronum", "Petrificus Totalus", "Muffliato" };
+			List<string> listOfSpells = new List<string> () { "Accio", "Expecto Patronum", "Petrificus Totalus", "Muffliato" };
 			string rememberSpell = "tr";
 			List<string> returnedSpells = new List<string> ();
-			listOfSpells.ForEach (element => {
-				if (element.Contains (rememberSpell)) returnedSpells.Add (element);
-			});
-			returnedSpells.ForEach(element => Console.WriteLine(element));
-
+			if(rememberSpell != ""){
+				listOfSpells.ForEach (element => {
+					if (element.Contains(rememberSpell) == true) returnedSpells.Add(element);
+				});
+				returnedSpells.ForEach (element => Console.WriteLine (element));
+			}
+			else{	Console.WriteLine ("Spell Does not Exist"); }
 
 			string message = "Have you finished your warewolves essay? ";
 			string secretMessage = message
-			.Replace('a','@')
-			.Replace('e','#')
-			.Replace('i','*')
-			.Replace('o','?')
-			.Replace('u','&');
-			Console.WriteLine(secretMessage); 
-		}
-
-
+				.Replace ('a', '@')
+				.Replace ('e', '#')
+				.Replace ('i', '*')
+				.Replace ('o', '?')
+				.Replace ('u', '&');
+			Console.WriteLine (secretMessage);
 	}
+
+}
 }
