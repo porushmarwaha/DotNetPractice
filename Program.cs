8bcc6dfd4e97ef626759c6f0bd3073e859a7e4b7
porushmarwaha <porushmarwaha@gmail.com>
2020-07-27 12:39:05 +0530

    New Branch is created for new problems solution

--- Program.cs
+++ Program.cs
@@ -1,25 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dotnet {
 	class Program {
 		static void Main (string[] args) {
 
-			Console.Write ("Please type number: ");
-			Console.WriteLine ("The Number is - {0}", EvenOddFunction (int.Parse (Console.ReadLine ())));
-
-			Console.Write("Plese Type String: ");
-			Console.WriteLine ("The Sum is - {0}", CalculateCommaSeperatedValues (Console.ReadLine()));
-
+			List<string> listOfSpells = new List<string> () 
+				{ "Accio", "Expecto Patronum", "Petrificus Totalus", "Muffliato" };
+			string rememberSpell = "tr";
+			List<string> returnedSpells = new List<string> ();
+			listOfSpells.ForEach (element => {
+				if (element.Contains (rememberSpell)) returnedSpells.Add (element);
+			});
+			returnedSpells.ForEach(element => Console.WriteLine(element));
 		}
-		private static int CalculateCommaSeperatedValues (string commaSeperatedString) {
-			int sum = 0;
-			foreach (string item in commaSeperatedString.Split (',')) {
-				if (int.Parse (item) < 0) continue;
-				sum += int.Parse (item);
-			}
-			return sum;
-		}
-		private static string EvenOddFunction (int number) { return (number % 2 == 0) ? "Even" : "Odd"; }
+
 
 	}
 }
